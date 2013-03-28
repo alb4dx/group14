509928a7b2f298d6d145957ae41242d88cdac283
jzambrotta <jzambrotta@10.0.1.25>
2013-03-28 04:48:46 -0400

    Added Java doc v1

--- ControlStation/src/control/data/InformationHandler.java
+++ ControlStation/src/control/data/InformationHandler.java
@@ -3,7 +3,16 @@ package control.data;
 import java.util.List;
 
 import javax.swing.JPanel;
-
+/**
+ * Used to hold all of the data received from the robot and does any necessary calculations
+ * @version 1.0 - Build 04/01/2013
+ * @author Stephanie Colen
+ * @author Sarina Padilla
+ * @author Hubert Chen
+ * @author Andy Barron
+ * @author John Zambrotta
+ *
+ */
 public class InformationHandler
 {
 	private int distance;
@@ -23,12 +32,23 @@ public class InformationHandler
 		//TODO
 	}
 	
+	/**
+	 * Graph update method
+	 * @return TBD
+	 */
 	public JPanel updateGraph()
 	{
 		//TODO
 		return null;
 	}
 	
+	/**
+	 * adds all telemetry sensor data to information handler
+	 * @param light		light sensor data
+	 * @param sound 	sound sensor data
+	 * @param ultrasonic	ultrasonic sensor data
+	 * @param touch		touch sensor data 
+	 */
 	public void addData(int light, int sound, int ultrasonic, boolean touch)
 	{
 		//TODO
