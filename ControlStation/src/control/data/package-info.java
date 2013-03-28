f33850b94c017c53a2c9c3b10640d5a654c94931
jzambrotta <jzambrotta@10.0.1.25>
2013-03-28 04:49:00 -0400

    Added Javadoc

--- ControlStation/src/control/data/package-info.java
+++ ControlStation/src/control/data/package-info.java
@@ -1,8 +1,11 @@
 /**
  * Package for classes used to gather and analyze telemetry data.
- */
-/**
+ * @version 1.0 - Build 04/01/2013
+ * @author Stephanie Colen
+ * @author Sarina Padilla
+ * @author Hubert Chen
  * @author Andy Barron
- * 
+ * @author John Zambrotta
+ *
  */
 package control.data;
